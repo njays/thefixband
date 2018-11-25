7c02592fcb0fcd6665e10fe7e1511ba7537c4a91
njays <njaysteele@gmail.com>
2018-11-25 20:38:30 +0000

    added javascript

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function contentAbout () {
+
+
+function contentAbout() {
+	document.getElementById('content').style.opacity = ".0";
+
+	setTimeout(function() {
 	document.getElementById("content").innerHTML =	"<div class='content'>" +
             "<h1>ABOUT</h1>" +
             "<p>Looking for a band that’s lively, high quality and vibrant? <b>The Fix</b> can provide you with live music that promises to entertain and get you up on your feet. Based in <b>Cardiff</b>, but willing to travel, <b>The Fix</b> play exciting arrangements of current music, performing them with style and energy. " + 
@@ -8,15 +13,28 @@ function contentAbout () {
         "</div>" +
         "<div class='content-2'>" +
         "    <img class='content-2__image' src='img/wideshot.png' alt='bandphoto'>" +
-        "</div>"
-
-    console.log("it worked");
-
+        "</div>";
 
 	document.getElementById('content').style.opacity = "1";
-	}
+	},500)};
 
-function contentClear () {
-	document.getElementById("content").innerHTML = "";
+function contentClear() {
 	document.getElementById('content').style.opacity = ".0";
-}
+};
+
+// function() {
+//   // Fade out
+//   project.style.opacity = 0;
+
+//   // Wait for the transition 
+//   setTimeout(function(){ 
+//       // Load new content
+//       projName.innerHTML = projects[projIndex].name;
+//       projDescr.innerHTML = projects[projIndex].description;
+//       projImg.src = projects[projIndex].img;
+//       projImg.style.width = '250px';
+//       projImg.style.height = '150px';
+//       projIndex = (projIndex + 1) % projects.length;
+//       // Fade in
+//       project.style.opacity = 1;
+//   },500);
